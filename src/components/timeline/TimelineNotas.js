4b4ef8994023d3ef370dcf350e779f72842a9b8c
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-06-07 13:44:55 -0300

    se limpio componente timelinenotas.js y se modifico notasView.js, modulo funcionando correctamente

--- src/components/timeline/TimelineNotas.js
+++ src/components/timeline/TimelineNotas.js
@@ -1,59 +1,4 @@
-// import { CopyOutlined } from "@ant-design/icons";
-// import { Empty, Timeline as TL } from "antd";
-// import React, { Fragment } from "react";
-// import NotaItem from "./NotaItem";
-
-// const TimelineNotas = ({ notes }) => {
-//   //console.log(" desde timeline: ", notes);
-
-//   return (
-//     <Fragment>
-//       {notes && notes.length > 0 ? (
-//         <div style={{height:"316px", width:"99%",overflowY:"scroll", padding:"15px"}}>
-//            <style>
-//             {`
-//               ::-webkit-scrollbar {
-//                 width: 8px;
-//               }
-
-//               ::-webkit-scrollbar-track {
-//                 background-color: #f5f5f5;
-//                 border-radius: 4px;
-//               }
-
-//               ::-webkit-scrollbar-thumb {
-//                 background-color: #e8e8e8;
-//                 border-radius: 4px;
-//               }
-
-//               ::-webkit-scrollbar-thumb:hover {
-//                 background-color: #e5e5e5;
-//               }
-//             `}
-//           </style>
-//           <TL>
-//             {notes.map((note) => (
-//               <TL.Item
-//                 style={{ width: "96.5%" }}
-//                 key={note.not_id}
-//                 dot={<CopyOutlined className="timeline-clock-icon" />}
-//               >
-//                 <NotaItem note={note} attached={false} />
-//               </TL.Item>
-//             ))}
-//           </TL>
-//         </div>
-//       ) : (
-//         <Empty
-//           image={Empty.PRESENTED_IMAGE_SIMPLE}
-//           description="No hay notas"
-//         />
-//       )}
-//     </Fragment>
-//   );
-// };
-// export default TimelineNotas;
-
+/* eslint-disable no-unused-vars */
 import { CopyOutlined } from "@ant-design/icons";
 import { Empty, Timeline as TL } from "antd";
 import React, { Fragment } from "react";
@@ -74,7 +19,7 @@ const TimelineNotas = ({ notes, card }) => {
       {filteredNotes && filteredNotes.length > 0 ? (
         <div
           style={{
-            height: "316px",
+            height: "210px",
             width: "99%",
             overflowY: "scroll",
             padding: "15px",
@@ -114,10 +59,7 @@ const TimelineNotas = ({ notes, card }) => {
           </TL>
         </div>
       ) : (
-        <Empty
-          image={Empty.PRESENTED_IMAGE_SIMPLE}
-          description="No hay notas"
-        />
+        ""
       )}
     </>
   );
