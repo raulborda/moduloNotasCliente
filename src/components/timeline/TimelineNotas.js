77d0cf743d3396b1aa79d2114cf1ff4c66e84327
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-06-08 11:57:45 -0300

    prueba de scroll

--- src/components/timeline/TimelineNotas.js
+++ src/components/timeline/TimelineNotas.js
@@ -1,73 +1,4 @@
-// /* eslint-disable no-unused-vars */
-// import { CopyOutlined } from "@ant-design/icons";
-// import { Empty, Timeline as TL } from "antd";
-// import React, { Fragment } from "react";
-// import NotaItem from "./NotaItem";
-
-// const TimelineNotas = ({ notes, card }) => {
-//   const filteredNotes = notes.filter((note) => {
-//     if (card === "general") {
-//       return note.not_anclado !== "1";
-//     } else if (card === "destacado") {
-//       return note.not_anclado !== "0";
-//     }
-//     return true;
-//   });
-
-//   return (
-//     <>
-//       {filteredNotes && filteredNotes.length > 0 ? (
-//         <div
-//           style={{
-//             height: "500px",
-//             width: "99%",
-//             overflowY: "scroll",
-//             padding: "15px",
-//           }}
-//         >
-//           <style>
-//             {`
-//             ::-webkit-scrollbar {
-//               width: 8px;
-//             }
-
-//             ::-webkit-scrollbar-track {
-//               background-color: #f5f5f5;
-//               border-radius: 4px;
-//             }
-
-//             ::-webkit-scrollbar-thumb {
-//               background-color: #e8e8e8;
-//               border-radius: 4px;
-//             }
-
-//             ::-webkit-scrollbar-thumb:hover {
-//               background-color: #e5e5e5;
-//             }
-//           `}
-//           </style>
-//           <TL>
-//             {filteredNotes.map((note) => (
-//               <TL.Item
-//                 style={{ width: "96.5%" }}
-//                 key={note.not_id}
-//                 dot={<CopyOutlined className="timeline-clock-icon" />}
-//               >
-//                 <NotaItem note={note} attached={false} />
-//               </TL.Item>
-//             ))}
-//           </TL>
-//         </div>
-//       ) : (
-//         ""
-//       )}
-//     </>
-//   );
-// };
-
-// export default TimelineNotas;
-
-
+/* eslint-disable no-unused-vars */
 import { CopyOutlined } from "@ant-design/icons";
 import { Empty, Timeline as TL } from "antd";
 import React, { Fragment } from "react";
@@ -90,7 +21,7 @@ const TimelineNotas = ({ notes, card }) => {
           style={{
             height: "500px",
             width: "99%",
-            overflow: "auto", // Cambio de "scroll" a "auto"
+            overflowY: "scroll",
             padding: "15px",
           }}
         >
@@ -135,3 +66,72 @@ const TimelineNotas = ({ notes, card }) => {
 };
 
 export default TimelineNotas;
+
+
+// import { CopyOutlined } from "@ant-design/icons";
+// import { Empty, Timeline as TL } from "antd";
+// import React, { Fragment } from "react";
+// import NotaItem from "./NotaItem";
+
+// const TimelineNotas = ({ notes, card }) => {
+//   const filteredNotes = notes.filter((note) => {
+//     if (card === "general") {
+//       return note.not_anclado !== "1";
+//     } else if (card === "destacado") {
+//       return note.not_anclado !== "0";
+//     }
+//     return true;
+//   });
+
+//   return (
+//     <>
+//       {filteredNotes && filteredNotes.length > 0 ? (
+//         <div
+//           style={{
+//             height: "500px",
+//             width: "99%",
+//             overflow: "auto", // Cambio de "scroll" a "auto"
+//             padding: "15px",
+//           }}
+//         >
+//           <style>
+//             {`
+//             ::-webkit-scrollbar {
+//               width: 8px;
+//             }
+
+//             ::-webkit-scrollbar-track {
+//               background-color: #f5f5f5;
+//               border-radius: 4px;
+//             }
+
+//             ::-webkit-scrollbar-thumb {
+//               background-color: #e8e8e8;
+//               border-radius: 4px;
+//             }
+
+//             ::-webkit-scrollbar-thumb:hover {
+//               background-color: #e5e5e5;
+//             }
+//           `}
+//           </style>
+//           <TL>
+//             {filteredNotes.map((note) => (
+//               <TL.Item
+//                 style={{ width: "96.5%" }}
+//                 key={note.not_id}
+//                 dot={<CopyOutlined className="timeline-clock-icon" />}
+//               >
+//                 <NotaItem note={note} attached={false} />
+//               </TL.Item>
+//             ))}
+//           </TL>
+//         </div>
+//       ) : (
+//         ""
+//       )}
+//     </>
+//   );
+// };
+
+// export default TimelineNotas;
